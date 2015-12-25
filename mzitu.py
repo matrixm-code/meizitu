cd2438c2f7fbd4784e1ff55d4ef39a39b9e1d8fb
lizhaoxing1 <276949696@qq.com>
2015-12-25 12:46:18 +0800

    add pool map and add time.sleep()

--- mzitu.py
+++ mzitu.py
@@ -7,34 +7,43 @@ import urllib2
 from bs4 import BeautifulSoup
 import threading
 
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
 #g_url=[]    #要爬取的url连接列表
 #g_imgurl=[]     #要爬去的img连接表
 class spiderUrl:
     def __init__(self,url):
         self.url = url
-        self.header = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36","Host":"www.mzitu.com"}
+        self.header = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36","Host":"www.mzitu.com","Connection":"keep-alive"}
+        #self.header = {"Host": "www.mzitu.com","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0","Accept": "image/png,image/*;q=0.8,*/*;q=0.5","Accept-Encoding": "gzip, deflate","Referer":self.url,"Connection": "keep-alive"}
         self.g_url= {}
         self.g_imgurl = []
         self.flage = ""
         self.name = ""
 ##保存为本地文件
     def saveImg(self,imgurl,filename):
-        data = urllib2.urlopen(imgurl).read()
-        f = open(filename,'wb')
-        f.write(data)
-        print "正在悄悄保存一张照片为:", filename
-        f.close()
+        try:
+            data = urllib2.urlopen(imgurl).read()
+            f = open(filename,'wb')
+            f.write(data)
+            print "正在悄悄保存一张照片为:", filename
+            f.close()
+        except urllib2.URLError, e:
+            print e.reason
+
+
 
     def saveInDir(self,name,imgurl):
         number = 1
-        name = name.encode("gbk")
+        #name = name.encode("gbk")
         print "共发现",len(imgurl),"张照片"
         for url in imgurl:
-            filename = "%s/%s(%d).jpg" % (name,name,number)
+            filename = "mzitu/%s/%s(%d).jpg" % (name,name,number)
             if os.path.isfile(filename):
                 print "已经爬过她了~~"
                 continue
             self.saveImg(url,filename)
+            time.sleep(0.1)
             number += 1
 
 
@@ -43,6 +52,7 @@ class spiderUrl:
 ##文件夹创建
     def MakeDir(self,path):
        path = path.strip()
+       path = "mzitu/%s" % (path)
        #判断路径下的文件夹是否存在
        isExists = os.path.exists(path)
        if not isExists:
@@ -95,20 +105,26 @@ class spiderUrl:
         g_url = self.getPageUrl(pagecode)
         for name,url in g_url.items():
             self.g_imgurl = []
-            self.MakeDir(name.encode("gbk"))
+            name = name.encode("gbk").replace("?","")
+            self.MakeDir(name)
             self.getImgUrl(url,url.split("/")[-1])
             self.saveInDir(name,self.g_imgurl)
-
-    def printUrl(self):
-        print self.g_imgurl
-
-
-
-
 
 
 
 if __name__ == "__main__":
-    url = "http://www.mzitu.com/xinggan"
-    spider = spiderUrl(url)
-    spider.getImg()
+    # url = "http://www.mzitu.com/xinggan"
+    # spider = spiderUrl(url)
+    # spider.getImg()
+
+    def Run(url):
+        spider = spiderUrl(url)
+        spider.getImg()
+    url = ["http://www.mzitu.com/xinggan/page/%d" % d for d in range(1,11)]
+
+    # for i in url:
+    #     Run(i)
+    pool = ThreadPool(4)
+    pool.map(Run,url)
+    pool.close()
+    pool.join()
